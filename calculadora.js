59daca5dec8a5eefa4bfa22ae3614c4ff13e8c98
mherferd <miguelhfdez@gmail.com>
2025-02-03 11:15:59 +0100

    Actualizacion

--- calculadora.js
+++ calculadora.js
@@ -5,6 +5,20 @@ function sumar(a, b) {
   function restar(a, b) {
     return a - b;
   }
+
+  function multiplicar(a, b) {
+    return a * b;
+  }
+
+  function dividir(a, b) {
+    return a / b;
+  }
+
+  function raiz(a) {
+    return  Math.sqrt(a);
+  }
+ 
+
+
   
-  
-  module.exports = { sumar, restar };
+  module.exports = { sumar, restar, multiplicar, dividir, raiz };
--- calculadora.test.js
+++ calculadora.test.js
@@ -1,9 +1,41 @@
-const { sumar, restar } = require('./calculadora');
+const { sumar, restar, raiz, dividir, multiplicar } = require('./calculadora');
 
+//SUMAS
 test('Suma 2 + 3 y devuelve 5', () => {
     expect(sumar(2, 3)).toBe(5);
 });
 
+test('Suma 7 + 13 y devuelve 20', () => {
+    expect(sumar(7, 13)).toBe(20);
+});
+
+
+//RESTAS
 test('Resta 5 - 3 y devuelve 2', () => {
     expect(restar(5, 3)).toBe(2);
 });
+
+test('Resta 7 - 13 y devuelve -6', () => {  
+    expect(restar(7, 13)).toBe(-6);
+});
+
+
+//MULTIPLICACION
+test('Multiplica 5 * 3 y devuelve 15', () => {
+    expect(multiplicar(5, 3)).toBe(15);
+});
+
+
+//DIVISION
+test('Divide 6 / 3 y devuelve 2', () => {
+    expect(dividir(6, 3)).toBe(2);
+});
+
+
+//RAIZ CUADRADA
+test('Raiz cuadrada de 9 y devuelve 3', () => {
+    expect(raiz(9)).toBe(3);
+});
+
+
+
